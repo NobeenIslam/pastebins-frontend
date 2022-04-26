e53b3f7f532eaabc84a73b53ca2d2e8705646803
Owen Dearman <owendearman@gmail.com>
2022-04-26 13:57:07 +0100

    fetch data from database in main component and add interface and base url

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,3 +1,20 @@
+import { baseUrl } from "../utils/baseUrl";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { pastesInterface } from "../utils/pastesInterface";
+
 export function MainContent(): JSX.Element {
-  return <div>Main Content</div>;
+  const [pastes, setPastes] = useState<pastesInterface[]>([]);
+
+  useEffect(() => {
+    const url = baseUrl + "/pastes";
+    async function fetchPastes() {
+      const response = await axios.get(url);
+      console.log(response.data);
+      setPastes(response.data);
+    }
+    fetchPastes();
+  }, []);
+
+  return <div></div>;
 }
