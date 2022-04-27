51e28bf75e5cdd74136f44f5747b978231b056a8
Nobeen <n.islam.1@outlook.com>
2022-04-27 16:59:06 +0100

    Created EditPasteForm and  conditionally render with button presses

---
+++ src/components/EditPasteForm.tsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { baseUrl } from "../utils/baseUrl";
+import { pastesInterface } from "../utils/pastesInterface";
+
+interface formDataInterface {
+  title: string;
+  text: string;
+}
+
+interface EditPasteFormProps {
+  changeToggle: (arg: boolean) => void,
+  toggle: boolean,
+  data: pastesInterface,
+  changeToggleEditForm: (arg: boolean) => void,
+}
+
+export function EditPasteForm(props: EditPasteFormProps): JSX.Element {
+  const [formData, setFormData] = useState<formDataInterface>({
+    title: props.data.title,
+    text: props.data.text
+  });
+
+
+  function handleFormChange(
+    event:
+      | React.ChangeEvent<HTMLTextAreaElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) {
+    const { name, value } = event.target;
+    setFormData((previous) => {
+      return { ...previous, [name]: value };
+    });
+    console.log(formData);
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    axios.put(baseUrl + `/pastes/${props.data.id}`, formData);
+    props.changeToggleEditForm(false)
+    props.changeToggle(!props.toggle)
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="paste-form-title">Title</label>
+      <input
+        name="title"
+        value={formData.title}
+        id="paste-form-title"
+        placeholder="Title"
+        type="text"
+        onChange={(e) => handleFormChange(e)}
+      />
+      <br />
+      <label htmlFor="paste-form-text">Text</label>
+      <textarea
+        name="text"
+        value={formData.text}
+        id="paste-form-text"
+        placeholder="Paste Text Here"
+        onChange={(e) => handleFormChange(e)}
+      />
+      <br />
+      <button>Submit</button>
+    </form>
+  );
+}
--- src/components/SinglePasteElement.tsx
+++ src/components/SinglePasteElement.tsx
@@ -3,6 +3,8 @@ import { pastesInterface } from "../utils/pastesInterface";
 import ShowMoreText from "react-show-more-text";
 import axios from "axios";
 import { baseUrl } from "../utils/baseUrl";
+import { EditPasteForm } from "./EditPasteForm";
+import { useState } from "react";
 
 interface SinglePasteElementProps {
   data: pastesInterface,
@@ -14,10 +16,17 @@ export function SinglePasteElement(
   props: SinglePasteElementProps
 ): JSX.Element {
 
+  const [showEditForm, setShowEditForm] = useState<boolean>(false)
+
   async function deletePaste(id: number) {
     await axios.delete(baseUrl + `/pastes/${id}`)
     props.changeToggle(!props.toggle)
   }
+
+  function editPaste() {
+    setShowEditForm(true)
+  }
+
   return (
     <section className="SinglePasteElement">
       {props.data.title && <p className="PasteTitle">{props.data.title}</p>}
@@ -27,7 +36,19 @@ export function SinglePasteElement(
       <p>Date Created: {creationDateFormatter(props.data.creationdate)}</p>
       <button
         onClick={() => deletePaste(props.data.id)}
-      >Delete</button>
+      >Delete
+      </button>
+      <button
+        onClick={editPaste}
+      >Edit
+      </button>
+      {showEditForm && <EditPasteForm
+        changeToggle={props.changeToggle}
+        toggle={props.toggle}
+        data={props.data}
+        changeToggleEditForm={setShowEditForm}
+
+      />}
     </section>
   );
 }
